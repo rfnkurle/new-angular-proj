4595e561e26d188cdea28650efb14e0bf5152144
robiwankenobi <rfnkurle@gmail.com>
2020-03-14 12:10:20 -0600

    reinitialized

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { MatButton, MatToolbar } from '@angular/material'
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,8 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'new-angular';
+
+  button(){
+    alert("Clicked")
+  }
 }
